b6816d9d9bbb5b250afb7b7284a9f68e80c0faed
sahurali <sahurali@localhost>
2024-08-30 23:56:10 -0400

    First Commit for assignment 1

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Name : Sahur Ali
+ */
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+
+	}
+
+}
